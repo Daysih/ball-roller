f64da5f8ca4e7e3dcf64a35cbae4850378e18a72
Daysi Hilario <dvh7@humboldt.edu>
2020-01-24 14:58:00 -0800

    ball game save

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+    }
+
+
+
+}
